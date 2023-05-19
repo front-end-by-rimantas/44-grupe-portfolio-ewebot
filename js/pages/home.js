0468a1e77070e08bb8620bc8304ca2abaa94c6a9
Kevinas <129725556+TadasRup@users.noreply.github.com>
2023-05-19 10:51:36 +0300

    Final commit #3

---
+++ js/pages/home.js
@@ -0,0 +1,29 @@
+/* <!-- HEADER: start--> */
+/* <!-- HEADER: end--> */
+
+/* <!-- HERO: start--> */
+/* <!-- HERO: end--> */
+
+/* <!-- CHOOSE US: start--> */
+/* <!-- CHOOSE US: end--> */
+
+/* <!-- METRICS: start--> */
+/* <!-- METRICS: end--> */
+
+/* <!-- VIDEO: start--> */
+/* <!-- VIDEO: end--> */
+
+/* <!-- SOCIALS: start--> */
+/* <!-- SOCIALS: end--> */
+
+/* <!-- SHOP BOOKS: start--> */
+/* <!-- SHOP BOOKS: end--> */
+
+/* <!-- BLOG: start--> */
+/* <!-- BLOG: end--> */
+
+/* <!-- CREATIVE: start--> */
+/* <!-- CREATIVE: end--> */
+
+/* <!-- FOOTER: start--> */
+/* <!-- FOOTER: end--> */
